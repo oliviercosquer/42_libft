c120010543b125295dad3555f251b52e86e886cd
olivier cosquer <olivier.cosquer@gmail.com>
2015-10-07 12:48:00 +0200

    Correction de ft_memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: olivier <olivier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 17:27:06 by ocosquer          #+#    #+#             */
-/*   Updated: 2015/07/06 00:57:02 by olivier          ###   ########.fr       */
+/*   Updated: 2015/10/07 12:47:12 by olivier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,14 @@ void	*ft_memmove(void *s1, const void *s2, size_t n)
 	str1 = (char*)s1;
 	str2 = ft_strdup(s2);
 	i = 0;
-	while (i < n)
+	if (str2)
 	{
-		str1[i] = str2[i];
-		i++;
+		while (i < n)
+		{
+			str1[i] = str2[i];
+			i++;
+		}
 		free(str2);
 	}
-	/*
-	if (str1 >= str2 && str1 <= (str2 + ft_strlen(str2)))
-	{
-		while (index < n)
-		{
-			str1[n - index - 1] = str2[n - index - 1];
-			index++;
-		}
-	}
-	else
-	{
-		while (index < n)
-		{
-			str1[index] = str2[index];
-			index++;
-		}
-	}*/
 	return (s1);
 }
