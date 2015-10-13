4b03eeba12a416d73cb8a41956a386e75b9e2908
Olivier COSQUER <ocosquer@e1r3p1.42.fr>
2015-10-13 18:38:46 +0200

    correction de ft_memccpy

--- srcs/ft_memccpy.c
+++ srcs/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: olivier <olivier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:17:23 by ocosquer          #+#    #+#             */
-/*   Updated: 2015/10/08 12:21:40 by olivier          ###   ########.fr       */
+/*   Updated: 2015/10/13 18:38:26 by ocosquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,19 @@ void	*ft_memccpy(void *str_dest, const void *str_src, int c, size_t n)
 	char			*dest;
 	char			*src;
 	char			character;
+	void			*tmp;
 
 	i = 0;
 	character = (char)c;
 	src = (char *)str_src;
 	dest = (char *)str_dest;
+	tmp = NULL;
 	while (src[i] && i < n && src[i] != character)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	if (src[i] == character)
-		return (&src[i]);
-	return (NULL);
+		tmp = &str_dest[i + 1];
+	return (tmp);
 }
