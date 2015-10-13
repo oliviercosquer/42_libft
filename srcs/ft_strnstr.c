a3499ee2291281a7bc243975da206db96907dbbb
Olivier COSQUER <ocosquer@e1r1p2.42.fr>
2015-10-13 21:07:09 +0200

    Correction de strnstr

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ocosquer <ocosquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 13:49:14 by ocosquer          #+#    #+#             */
-/*   Updated: 2013/12/01 20:37:21 by ocosquer         ###   ########.fr       */
+/*   Updated: 2015/10/13 21:06:38 by ocosquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,26 @@
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	char	*str_found;
-	size_t	i_s1;
-	size_t	i_s2;
-	size_t	l_s1;
-	size_t	l_s2;
+	char	*found;
+	size_t	i;
+	size_t	j;
 
-	str_found = 0;
-	i_s1 = 0;
-	i_s2 = 0;
-	l_s1 = ft_strlen((char*)s1);
-	l_s2 = ft_strlen((char*)s2);
-	str_found = (l_s2 == 0) ? (char*)s1 : 0;
-	while ((i_s1 < l_s1) && str_found == 0 && i_s1 < n)
+	found = NULL;
+	i = 0;
+	if (ft_strlen((char *)s2) == 0)
+		return ((char *)s1);
+	while (s1[i] && !found && i < n)
 	{
-		str_found = &((char*)s1)[i_s1];
-		while ((i_s2 < l_s2) && (i_s2 + i_s1) < n)
+		found = (char *)s1 + i;
+		j = 0;
+		while (s2[j] && (i + j) < n)
 		{
-			str_found = (s1[i_s1 + i_s2] == s2[i_s2]) ? str_found : 0;
-			i_s2++;
+			found = (s1[i + j] == s2[j]) ? found : NULL;
+			j++;
 		}
-		i_s2 = 0;
-		i_s1++;
+		i++;
 	}
-	str_found = (ft_strlen((char*)s2) > n) ? 0 : str_found;
-	return (str_found);
+	if (j < ft_strlen((char *)s2))
+		return (NULL);
+	return (found);
 }
